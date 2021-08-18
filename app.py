1dff525604fedabdd21f58789c5e11c73e4ee102
Gaurisha21 <68791455+Gaurisha21@users.noreply.github.com>
2021-08-18 22:46:48 +0530

    updated app.py

--- app.py
+++ app.py
@@ -20003,8 +20003,8 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
+    return '''<h1>Memes Api</h1>
+<p>A prototype API random memes.</p>'''
 
 
 @app.route('/api/memes/all', methods=['GET'])
